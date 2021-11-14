12051378ea35025e4d1f96767c0d8af56e98109f
francescoanzi <92693084+francescoanzi@users.noreply.github.com>
2021-11-14 20:58:43 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,18 @@
 #include <iostream>
-
 using namespace std;
-
-int main() {
-
-int n=0, primo=1, secondo=1, fibonacci=0;
-
+int main ()
+        int n=0, primo=1,secondo=1, fibonacci=0;
 cin>> n;
-
-        if(n<=2){
-            cout<<"errore"<<endl;
-        }else{
-            cout<< "1" << "\n" << "1" << endl;
-
-            for (int i = 2; i < n; ++i) {
+if(n<=2){
+        cout<<"errore"<<endl;
+}else{
+        cout<<"1"<< "/n" << "1" << endl;
+        for(int i=2; i<n; ++i) {
                 fibonacci=primo+secondo;
                 primo=secondo;
                 secondo=fibonacci;
-                cout<< fibonacci <<endl;
-            }
+                cout << fibonacci << endl;
         }
-
-    return 0;
+}
+return 0;
 }
