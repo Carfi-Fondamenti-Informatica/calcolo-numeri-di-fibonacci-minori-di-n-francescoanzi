d32261bf20868aa06de532fbd77a5dcabd0ed298
francescoanzi <92693084+francescoanzi@users.noreply.github.com>
2021-11-14 20:14:53 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n=0, primo=1, secondo=1, terzo=0, cont=0 ;
+int n=0, cont=1, serie=0, primo=1, secondo=1, terzo=0 ;
 
-    cin >> n ;
-    if (n <=0) {
+        cin >> n ;
+         cont++;
+                if (n<=2){
+                    cout << "errore";
+                }else{
+                    cout << "1" << endl;
+                    cout << "1" << endl;
+                        for (cont = 2; cont < n; cont++) {
+                                terzo = primo + secondo;
+                                cout << terzo << endl;
+                                primo = secondo;
+                                secondo = terzo;
+
+
+                            }
+
+
+    }
+
         return 0;
-    }
-    else {
-        cout << "1" << endl;
-        cout << "1" << endl;
-        for (cont = 2; cont < n; cont++) {
-            terzo = primo + secondo;
-            primo = secondo;
-            secondo = terzo;
-            if (terzo<=n) {
-                cout << terzo << endl;
-            }
-            else{
-                return 0;
-            }
-
-        }
-    }
-    }
-
-
-
-
-
-
+}
